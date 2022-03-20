5e30700ea9c2f19de2de5320f3e0e4ad3557296d
RomanBuryy <roman.buriy@gmail.com>
2022-03-20 21:19:20 +0200

    add some exercises

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
 import './App.css';
 import Users from "./Users/Users";
+import {useEffect, useState} from "react";
+import {userService} from "./services/user.service";
+import User from "./User/User";
 
 function App() {
 
+    //Simple form
+    const formHandler = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
+
+    const send = (e) => {
+        e.preventDefault();
+        console.log(form)
+    }
+   //////////////////////////////////////////
+    const [form, setForm] = useState({name: '', age: 1});
+    const[users, setUsers]=useState([]);
+    const[userId, setUserId] = useState(0);
+    const[user, setUser] = useState(null);
+
+    useEffect(() => {
+        userService.getAll().then(value => setUsers(value))
+    }, [])
+
+    const formHandlerOptions = (e) => {
+        const id = e.target.value;
+        setUserId(id);
+    }
+
+    const sendOption = (e) =>{
+        e.preventDefault();
+        userService.getById(userId).then(value => setUser(value));
+    }
 
     return (
         <div>
-            <Users/>
+            {/*<Users/>*/}
+            <div>
+                <form onSubmit={send}>
+                    <label>Name: <input type="text" name={'name'} value={form.name} onChange={formHandler}/></label>
+                    <label>Age: <input type="number" name={'age'} value={form.age} onChange={formHandler}/></label>
+                    <button>Submit</button>
+                </form>
+            </div>
+
+            <div>
+                <form onSubmit={sendOption}>
+                    <select name={'userId'} onChange={formHandlerOptions}>
+                        {users.map(value => <option key={value.id} value={value.id}>{value.name}</option>)}
+                    </select>
+                    <button>Submit</button>
+                </form>
+            </div>
+
+            {user && <User user={user}/> }
+
         </div>);
 }
 
